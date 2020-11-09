0d0d5488d57a06fbac62af3f23417cc4f072be7b
Wene Alves <weneplay5@gmail.com>
2020-11-09 18:07:44 -0300

    trabalho concluido em fisica

--- src/index.js
+++ src/index.js
@@ -8,10 +8,35 @@ app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
     extended: false
 }))
+
+let resultado = 0
+const calculo = (unidade1,unidade2, qA, qB, D) => {
+    const K = 9 * (Math.pow(10,9))
+    let F = 0
+    const unidade = {
+        mc: Math.pow(10,-3),
+        uc: Math.pow(10,-6),
+        nc: Math.pow(10,-9), 
+    }
+
+    return F = (K * qA * unidade[unidade1] * qB * unidade[unidade2]) / Math.pow(D,2) 
+    
+}
+
 app.use(bodyParser.json())
 
 app.get('/',(req, res) =>{
-    res.render('index')
+    res.render('index',{
+        resultado
+    })
+})
+
+app.post('/responder',(req, res) =>{
+    console.log(req.body)
+    resultado = calculo(req.body.unidade1, req.body.unidade2, parseFloat(req.body.qA), parseFloat(req.body.qB),parseFloat(req.body.distancia))
+    res.render('index',{
+        resultado
+    })
 })
 
 
