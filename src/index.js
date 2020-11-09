83f3e58a0943f366780e9501c49fee9390019bda
Wene Alves <weneplay5@gmail.com>
2020-11-09 14:54:27 -0300

    modifiquei o index

--- src/index.js
+++ src/index.js
@@ -4,7 +4,7 @@ const app = express()
 const bodyParser = require('body-parser')
 
 app.set('view engine', 'ejs')
-app.use(express.static('/public'))
+app.use(express.static('public'))
 app.use(bodyParser.urlencoded({
     extended: false
 }))
